9aa5601ca6c9bced6997c0a25923883c434f3f4b
abhinav kiran <abhinav kiran>
2020-06-08 20:32:36 +0530

    added file

--- db_connection.js
+++ db_connection.js
@@ -1,7 +1,32 @@
 var mysql = require('mysql');
-exports.connection = mysql.createConnection({
+var GetJsonData;
+/* exports.connection = mysql.createConnection({
   localhost: "3306",
   user: "root",
   password: "root",
   database: "mydb"
 });
+ */
+var connection = mysql.createConnection({
+  localhost: "3306",
+  user: "root",
+  password: "root",
+  database: "mydb"
+});
+
+function create_db(){
+	
+}
+
+function Delete_db(){
+	
+}
+
+exports.Fetch_db = function(sql){
+	connection.query(sql, function (err, result) {
+	  GetJsonData=JSON.stringify(result);
+    if (err) throw err;
+    console.log("query executed");
+  });
+  return GetJsonData;
+}
